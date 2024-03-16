57889a76a4ea5a129e8e75318ed6f19545dc3986
gavinearley <gavinearley@outlook.com>
2024-03-15 22:12:09 -0400

    Make footer year dynamic

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -3,6 +3,6 @@
     class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
   >
     <br>
-    <p>© 2024 Gavin Earley</p>
+    <p>© {{ new Date().getFullYear() }} Gavin Earley</p>
   </footer>
 </template>
